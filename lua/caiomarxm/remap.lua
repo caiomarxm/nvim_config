4de389e55272a7ae74d0aac2fabe6dca6ea77659
Caio Martins <caiomarxm@gmail.com>
2022-09-12 07:45:05 -0300

    feat: nvim-tree map and auto pairs

--- lua/caiomarxm/remap.lua
+++ lua/caiomarxm/remap.lua
@@ -1,4 +1,5 @@
 local nnoremap = require("caiomarxm.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
-
+nnoremap("<leader>t", "<cmd>NvimTreeOpen<CR>")
+nnoremap("<leader>c", "<cmd>NvimTreeClose<CR>")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["auto-pairs"] = {
+    loaded = true,
+    path = "/home/caiom/.local/share/nvim/site/pack/packer/start/auto-pairs",
+    url = "https://github.com/jiangmiao/auto-pairs"
+  },
   ["dracula.nvim"] = {
     loaded = true,
     path = "/home/caiom/.local/share/nvim/site/pack/packer/start/dracula.nvim",
