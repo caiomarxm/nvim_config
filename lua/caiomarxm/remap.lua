9f940bb8a483b7b9f70161b3ff1df9b4345b8afe
Caio Martins <caiomarxm@gmail.com>
2022-09-12 08:27:46 -0300

    update: remaps

--- lua/caiomarxm/remap.lua
+++ lua/caiomarxm/remap.lua
@@ -1,6 +1,6 @@
 local nnoremap = require("caiomarxm.keymap").nnoremap
 
-nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<leader>t", "<cmd>NvimTreeOpen<CR>")
 nnoremap("<leader>c", "<cmd>NvimTreeClose<CR>")
 nnoremap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+
