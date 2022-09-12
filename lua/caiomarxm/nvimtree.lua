4b6c7d78df091c77f4aadeb3dcbccc5ea00c6bec
Caio Martins <caiomarxm@gmail.com>
2022-09-12 09:42:56 -0300

    feat: git ignore

--- lua/caiomarxm/nvimtree.lua
+++ lua/caiomarxm/nvimtree.lua
@@ -2,5 +2,10 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    git = {
+        enable = true,
+        ignore = false,
+    }
+})
 
