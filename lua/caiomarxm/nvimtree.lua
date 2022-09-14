b724b01b045683c843d45ba8b3cc5c985a6c7184
Caio Martins <caiomarxm@gmail.com>
2022-09-14 09:16:22 -0300

    feat: nvim-tree highlights

--- lua/caiomarxm/nvimtree.lua
+++ lua/caiomarxm/nvimtree.lua
@@ -1,11 +1,15 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
 require("nvim-tree").setup({
     git = {
         enable = true,
         ignore = false,
-    }
+    },
+    update_focused_file = {
+        enable = true,
+        update_root = true,
+        ignore_list = {},
+    },
 })
 
--- lua/caiomarxm/remap.lua
+++ lua/caiomarxm/remap.lua
@@ -1,6 +1,9 @@
 local nnoremap = require("caiomarxm.keymap").nnoremap
 
-nnoremap("<leader>t", "<cmd>NvimTreeOpen<CR>")
+nnoremap("<leader>o", "<cmd>NvimTreeOpen<CR>")
 nnoremap("<leader>c", "<cmd>NvimTreeClose<CR>")
+nnoremap("<leader>t", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>nr", "<cmd>NvimTreeRefresh<CR>")
+nnoremap("<leader>nf", "<cmd>NvimTreeFindFile<CR>")
 nnoremap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 
