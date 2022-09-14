37f7c46d8e4763a84d5bfc0be4c30ce451177ab7
Caio Martins <caiomarxm@gmail.com>
2022-09-14 11:08:27 -0300

    feat: treesitter and autotags

--- lua/caiomarxm/init.lua
+++ lua/caiomarxm/init.lua
@@ -9,5 +9,5 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+--require("nvim-tree").setup()
 
--- lua/caiomarxm/lspconfig.lua
+++ lua/caiomarxm/lspconfig.lua
@@ -52,6 +52,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 require('lspconfig')['html'].setup{capabilities=capabilities}
 require('lspconfig')['cssls'].setup{capabilities=capabilities}
+require('nvim-ts-autotag').setup()
 
 
 -- cmp and snippets
